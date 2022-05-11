8d036f3872ee4d07b8a8269fd9bf842308478f36
Benjamin Danishevsky <ben.danishevsky@gmail.com>
2022-05-11 14:08:52 -0400

    setup for splash page. file created, showing in browser

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+
 import LoginForm from './components/auth/LoginForm';
 import SignUpForm from './components/auth/SignUpForm';
 import NavBar from './components/NavBar';
@@ -8,6 +9,8 @@ import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
 import { authenticate } from './store/session';
+
+import SplashPage from './components/SplashPage';
 
 function App() {
   const [loaded, setLoaded] = useState(false);
@@ -43,6 +46,10 @@ function App() {
         <ProtectedRoute path='/' exact={true} >
           <h1>My Home Page</h1>
         </ProtectedRoute>
+
+        <Route exact path='/splash'>
+          <SplashPage />
+        </Route>
       </Switch>
     </BrowserRouter>
   );
---
+++ react-app/src/components/SplashPage/index.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useHistory } from 'react-router-dom';
+import './SplashPage.css';
+
+function SplashPage(){
+
+
+
+    return (
+        <>
+            <h1>Splash Page!!!</h1>
+        </>
+    )
+}
+
+
+export default SplashPage;
