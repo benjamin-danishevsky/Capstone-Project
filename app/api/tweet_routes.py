c72139b33bf3b18682439f153b1ea6ddd91c538b
Benjamin Danishevsky <ben.danishevsky@gmail.com>
2022-05-11 13:17:47 -0400

    fixed text in tweet seeders, route to get all tweets working

---
+++ app/api/tweet_routes.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, Flask, jsonify, session, request
+from app.models import db, User, Tweet
+from flask_login import login_required, current_user
+from datetime import datetime
+
+tweet_routes = Blueprint('tweets', __name__)
+
+
+@tweet_routes.route('/')
+def get_all_tweets():
+    tweets = Tweet.query.all()
+    return {
+        "tweets": [tweet.to_dict() for tweet in tweets]
+    }
