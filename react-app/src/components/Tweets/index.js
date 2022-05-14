26a36e14ee21d67a21276fe6193a3b5d167fab18
Benjamin Danishevsky <ben.danishevsky@gmail.com>
2022-05-14 16:47:15 -0700

    got tweet owner usernames to show on home page

--- react-app/src/components/Tweets/index.js
+++ react-app/src/components/Tweets/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import * as tweetActions from '../../store/tweets'
+import * as userActions from '../../store/users'
 
 const Tweets = () => {
     const history = useHistory();
@@ -9,11 +10,13 @@ const Tweets = () => {
 
     useEffect(() => {
         dispatch(tweetActions.getAllTweetsThunk())
+        dispatch(userActions.getAllUsersThunk())
     }, [dispatch])
 
     const sessionUser = useSelector(state => state.session.user)
     const tweets = useSelector(state => state.tweets)
     const tweetsData = Object.values(tweets)
+    const users = useSelector(state => state.users)
 
 
     return (
@@ -23,6 +26,7 @@ const Tweets = () => {
                     <div key={idx}>
                         <div>
                             <ul>
+                                <li>@{users[tweet.user_id].username}</li>
                                 <li>{tweet.text}</li>
                                 <li>Posted: {tweet.created_at}</li>
                             </ul>
